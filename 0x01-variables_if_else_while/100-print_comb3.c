2f3c80c5429ca0f9b9c782fcf4ddb161e2d6e1a3
00CrystalJ00 <okerekejennifer1509@gmail.com>
2022-10-14 23:18:37 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,26 +9,29 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != j)
+		for (j = k; j <= '9'; j++)
 		{
+			if (i != j)
+			{
 			putchar(i);
 			putchar(j);
-		}
-		if (i == j)
-		{
-			continue;
-		}
-		if (i == '8' && j == '9')
-		{
-			break;
-		}
-		else
-		{
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
 			putchar(',');
 			putchar(' ');
+			}
 		}
+		k++;
 	}
-	k++;
 	putchar('\n');
 	return (0);
 }
