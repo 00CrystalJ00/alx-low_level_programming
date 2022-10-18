ae6a1ee59b1a6cd5454a4bbd0c8ef23259404f0e
00CrystalJ00 <okerekejennifer1509@gmail.com>
2022-10-18 15:38:49 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and new line
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
@@ -17,4 +17,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+}
 
