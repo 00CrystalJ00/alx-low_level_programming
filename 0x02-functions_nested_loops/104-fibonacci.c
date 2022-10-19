a10121af92ee74c3561572f134df5ae987362fdb
00CrystalJ00 <okerekejennifer1509@gmail.com>
2022-10-19 15:32:30 -0700

    late

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * numLength - returns string length
+ * @num: operand number
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+		return (1);
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+
+/**
+ * main - Entry point
+ * Description: first 98 fibonacci num with 1 and 2 followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, i;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (n = 1; n <= 98; ++n)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		i = numLength(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 && i > 0)
+		{
+			printf("%d", 0);
+			--i;
+		}
+
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2);
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (n != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,29 +1,36 @@
-#include "main.h"
-
+#include"main.h"
 /**
- * times_table - print the 9 times table
+ * times_table - prints the 9 times table
+ * Example Table
+ * 0, 0, 0, 0, ..
+ * 0, 1, 2, 3, ..
  */
+
 void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; ++num)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; num++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
 		{
 			_putchar(',');
-			_putchar(' '');
+			_putchar(' ');
 
-			prod = num + mult;
+			prod = num * mult;
+
+			/**
+			 * put space if product is a single number
+			 * if two numbers place first digit
+			 */
 
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
+				_putchar((prod / 10) + 48);
 
-			_putchar((prod % 10) + '0');
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
